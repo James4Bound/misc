262ca9fb3090c1c8fd8dbdaf8c5c913f127fa0fd
James4Bound <42110103+James4Bound@users.noreply.github.com>
2021-02-28 12:27:25 +0800

    1

--- configure.sh
+++ configure.sh
@@ -9,6 +9,10 @@ install -m 755 /tmp/server/proxy-multi.conf /usr/local/bin/proxy-multi.conf
 install -m 755 /tmp/server/proxy-secret /usr/local/bin/proxy-secret
 
 # Remove temporary directory
-rm -rf /tmp/server
+#rm -rf /tmp/server
 
-/usr/local/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S "76c59bce782e456cc4af380c50ecddae" --aes-pwd /usr/local/bin/proxy-secret /usr/local/bin/proxy-multi.conf -M 1
+#/usr/local/bin/mtproto-proxy -u nobody -p 8888 -H 443 -S "76c59bce782e456cc4af380c50ecddae" --aes-pwd /usr/local/bin/proxy-secret /usr/local/bin/proxy-multi.conf -M 1
+
+chmod +x /tmp/server/mtproto-proxy
+cd /tmp/server
+./mtproto-proxy -u nobody -p 8888 -H 443 -S "76c59bce782e456cc4af380c50ecddae" --aes-pwd proxy-secret proxy-multi.conf -M 1
